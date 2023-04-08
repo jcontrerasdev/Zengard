a36407cd14cf29c1108b767f6dfff428f6dbbd1e
Juan Carlos Contreras <cmjcarlos7@gmail.com>
2023-04-09 00:59:11 +0200

    App created.

---
+++ zengard/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace zengard;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
